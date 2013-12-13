636bc9ee0353e217ba4d2a71001466aab2558760
xnnyygn <xnnyygn@gmail.com>
2013-12-13 17:22:51 +0800

    add deployingrails

---
+++ deployingrails/massiveapp/lib/deploy/db.rb
@@ -0,0 +1,6 @@
+namespace :deploy do
+  desc "Copy the database.yml file into the latest release"
+  task :copy_in_database_yml do
+    run "cp #{shared_path}/config/database.yml #{latest_release}/config/"
+  end
+end
---
+++ deployingrails/massiveapp_ops/manifests/nodes.pp
@@ -0,0 +1,6 @@
+node "app" {
+  include apache2
+  include mysql
+  include massiveapp
+  include passenger
+}
